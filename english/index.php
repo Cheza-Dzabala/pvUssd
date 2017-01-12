343bf866e287434d72c0cb4cc9a3afc7371d77be
Cheza-Dzabala <dzabalamacheza@gmail.com>
2017-01-12 15:54:37 +0200

    Initial Commit

---
+++ english/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheza
+ * Date: 1/12/2017
+ * Time: 3:40 PM
+ */
+    include "register/index.php";
+
+    /* First Level Logic */
+    $englishResponse = "CON What would you like to do? \n";
+    $englishResponse .= "1. Register Yourself\n";
+    $englishResponse .= "2. Report ADR \n";
+    $englishResponse .= "3. Enquire \n";
+
+?>
---
+++ english/register/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cheza
+ * Date: 1/12/2017
+ * Time: 3:44 PM
+ */
+    $registerResponse = "CON Is this $phoneNumber your phone number? \n";
+    $registerResponse .= "1. Yes";
+    $registerResponse .= "2. No";
+?>
+
